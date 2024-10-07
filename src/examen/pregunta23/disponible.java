bbd38d04b8adb1d237fc4da957430bfc7ef89d7c
MishelEspinoza07 <102001751+MishelEspinoza07@users.noreply.github.com>
2024-10-07 18:24:33 -0400

    tercer commit parcial

---
+++ src/examen/pregunta23/disponible.java
@@ -0,0 +1,8 @@
+package examen.pregunta23;
+
+public class Disponible {
+    public boolean Disponible(String modelo, String marca){
+        return true;
+    }
+
+}
---
+++ src/examen/pregunta23/register.java
@@ -0,0 +1,9 @@
+package examen.pregunta23;
+
+public class register {
+        public boolean registrarPrestamo(String modelo, String marca, String usuario){
+            return true;
+        }
+
+    }
+
